afdaa1790c4abf3275fbbbbeaab2752f49c0ad27
Hasan Mohammad Al-oqool <45208212+hasanoqool@users.noreply.github.com>
2023-11-30 10:51:59 +0000

    fully convolutional network for image segmentation

--- FCN.py
+++ FCN.py
@@ -1,1 +1,16 @@
-print("DD")
+import pathlib
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+from tensorflow.keras.layers import *
+from tensorflow.keras.losses import \
+    SparseCategoricalCrossentropy
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import RMSprop
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
